b56f430bd670db624d446abaf7d8691c41383040
susano99 <aloushi1999@hotmail.com>
2023-06-24 18:08:40 +0300

    add allow origin

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(
   cors({
     origin: [
